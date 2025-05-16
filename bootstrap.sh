3f53a7060d9db088502548be3560435d72f261f5
Bondaliname <lina@Lenovo-Thinkpad.>
2025-05-16 13:12:02 +0000

    2

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-kubectl apply -f pv.yml
+kubectl apply -f .infrastructure/pv.yml
 
-kubectl apply -f pvc.yml
+kubectl apply -f .infrastructure/pvc.yml
 
-kubectl apply -f configmap.yml
+kubectl apply -f .infrastructure/onfigmap.yml
 
-kubectl apply -f secrets.yml
+kubectl apply -f .infrastructure/secrets.yml
 
-kubectl apply -f deployment.yml
+kubectl apply -f .infrastructure/deployment.yml
 
 kubectl rollout status deployment/todoapp
